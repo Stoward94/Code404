b82cd24df09a07fdf5378914013256bae4922a50
Luke Stoward <luke.stoward@students.plymouth.ac.uk>
2015-10-18 15:13:38 +0100

    Added Code404 website files to the repo

---
+++ Scripts/site.js
@@ -0,0 +1,14 @@
+﻿$(function() {
+
+    $('.nav-item').click(function () {
+        $('.nav-active').removeClass("nav-active");
+        $(this).addClass("nav-active");
+    });
+
+    $('.nav-item').click(function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500);
+        return false;
+    });
+});
